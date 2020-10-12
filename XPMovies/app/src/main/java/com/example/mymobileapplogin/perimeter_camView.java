395961b34beb39e535c14372023cb406b85269ce
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-10-12 01:25:28 -0400

    Added Switch view button functionality and fixed sign out button.

--- XPMovies/app/src/main/java/com/example/mymobileapplogin/perimeter_camView.java
+++ XPMovies/app/src/main/java/com/example/mymobileapplogin/perimeter_camView.java
@@ -2,18 +2,23 @@ package com.example.mymobileapplogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class perimeter_camView extends AppCompatActivity {
+    Button switchViewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perimeter_cam_view);
 
+        switchViewButton = findViewById(R.id.switchViewButton);
         VideoView videoView = findViewById(R.id.video_view);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
         Uri uri = Uri.parse(videoPath);
@@ -22,5 +27,14 @@ public class perimeter_camView extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
+        switchViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity_portal.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
