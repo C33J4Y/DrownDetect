704b2e675851b1280c7b71415219f1096e7968d6
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-10-20 23:39:38 -0400

    Added Java class and layout for pool cam view.

---
+++ XPMovies/app/src/main/java/com/example/mymobileapplogin/pool_camView.java
@@ -0,0 +1,40 @@
+package com.example.mymobileapplogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class pool_camView extends AppCompatActivity {
+    Button switchViewButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pool_cam_view);
+
+        switchViewButton = findViewById(R.id.switchViewButton);
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        switchViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity_portal.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
