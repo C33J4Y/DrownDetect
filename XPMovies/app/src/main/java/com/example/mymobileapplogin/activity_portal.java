179b27255f846073b4372e9db551696b306fec69
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-10-22 00:57:39 -0400

    Implemented notifications in activity portal.

--- XPMovies/app/src/main/java/com/example/mymobileapplogin/activity_portal.java
+++ XPMovies/app/src/main/java/com/example/mymobileapplogin/activity_portal.java
@@ -1,17 +1,23 @@
 package com.example.mymobileapplogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.example.mymobileapplogin.Notifications.CHANNEL_1_ID;
+
 public class activity_portal extends AppCompatActivity {
     TextView signOutButton;
     Button perimeterCamButton;
     Button poolCamButton;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,7 @@ public class activity_portal extends AppCompatActivity {
         signOutButton = findViewById(R.id.textSignOut);
         perimeterCamButton = (Button) findViewById(R.id.perimeterButton);
         poolCamButton = (Button) findViewById(R.id.poolButton);
+        notificationManager = NotificationManagerCompat.from(this);
 
 
         perimeterCamButton.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +58,21 @@ public class activity_portal extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        }
+
+    public void sendOnChannel1(View v) {
+        //String title = editTextTitle.getText().toString();
+        //String message = editTextMessage.getText().toString();
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_warning)
+                .setContentTitle("ALERT")
+                .setContentText("Possible drowning. Please assist person.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManager.notify(1, notification);
 
 
     }
